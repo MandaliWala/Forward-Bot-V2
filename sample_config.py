509d740ed59759082bad791fa2130f29273e8e32
MandaliWala <141039684+MandaliWala@users.noreply.github.com>
2024-06-23 22:04:24 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,9 +1,9 @@
 class Config(object):
-    APP_ID = 000
-    API_HASH = "000"
-    BOT_TOKEN = "000"
+    APP_ID = 25062134
+    API_HASH = "1c2c6d7244d4576b2baab88337b1233a"
+    BOT_TOKEN = "7016509767:AAHGyELmvA9D6uCz2yPsI5sL4gr7pooU1lI"
     BOT_WORKERS = 40
-    SUDO_USERS = "000,111,222,333"
-    DB_URI = "mongo...."
-    SESSION_NAME = "test"
-    MULTI_USER_MODE = False
+    SUDO_USERS = "6580023819,6302921275"
+    DB_URI = "mongodb+srv://File2Link:File2Link@cluster0.ygj9iuu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    SESSION_NAME = "BQF-avYAlQgtXTKopvbm11qjM7VXdyymyAcvyqbVxPsPoKuI_h4kfGL71XQmRvvAfQdeEjGlPCr0ScvmUWqyv0TgOnfh3szxTYnyj2EKGElFsfeJA4RNAYFo9gAlP21SIxsOnxVShpcnWZwOU2K-68aL_5ATrJWqcY_1GDQhKSIiih6OUw4iRl-bQOQvseCSK_Rtg4_PzkLesUbYCzI7KakYSj_k0G9ZKzDc9eknY4XlPxQvLCnY04gRE5WSz2YziAhjUuleoowICTz-Ji5fTzmriBX4vKEiwiO5W20Hk1BdZsT1R3TSQB-MV4YdMM8KRiNU-NWlDZXQB3uUvldqQn98IIJ_6wAAAAGIMzILAA"
+    MULTI_USER_MODE = True
